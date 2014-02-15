98ea2f0c9bf5f5c35b1c1ecd4d3f1e1f27764234
tenguyasiki <motohasystem@gmail.com>
2014-02-15 16:51:49 +0900

    sharer.phpへのリンクを付ける

--- js/ysb.js
+++ js/ysb.js
@@ -1,13 +1,22 @@
 jQuery(document).ready( function($){
-	var ysb = YetSirButton;
-	
 	// 全ての記事ノードについて下記の処理を行い、いいね件数を表示させる
+	var ysb = YetSirButton;
 	$('.ysbAnchor').each( function(i){
 		var anchorNode = this;
 		ysb.getCount().done( function(json){
 			var url = json.id;
 			var count = json.shares;
-			$(anchorNode).append("<div>count : " + count + "</div>" );
+			var buttonText;
+			if(count > 0)
+			{
+				buttonText = "やっとやっと！";
+			}
+			else
+			{
+				buttonText = "やっとさー！";
+			}
+			var anchorTag = ysb.composeAnchorTag(url, "", buttonText);
+			$(anchorNode).append(anchorTag);
 		})
 		.fail( function(e){
 			console.log("request failed : " + e);
@@ -17,13 +26,23 @@ jQuery(document).ready( function($){
 
 var YetSirButton = {};
 
+// やっとさーボタン用のタグを構築して返す。
+YetSirButton.composeAnchorTag = function(url, title, text){
+	var tag = '<a href="http://www.facebook.com/sharer.php?u=' + url 
+		+ '&t=' + title
+		+ '" target="_blank" rel="nofollow">'
+		+ text  + '</a>';
+	return tag;
+};
+
 // graph apiを呼び出していいね件数を取得する
 YetSirButton.getCount = function(url){
 	var dfr = $.Deferred();
 	
 	// graph api
 	// http://graph.facebook.com/?id=http://hello-apis.blogspot.jp/2013/02/facebookapi.html
-	var url = "http://hello-apis.blogspot.jp/2013/02/facebookapi.html";
+	//var url = "http://creators-db.net/entry/";
+	var url = location.href;
 	
 	var api_url = "http://graph.facebook.com/?id=" + url;
 	$.ajax({
