0fe1285d8e94196602d326351b4be19b2b69a782
tenguyasiki <motohasystem@gmail.com>
2014-02-15 18:22:45 +0900

    #1 やっとさーとやっとやっとのロジックを分離（やっとやっとはまだ動作せず）

--- js/ysb.js
+++ js/ysb.js
@@ -6,9 +6,13 @@ jQuery(document).ready( function($){
 		ysb.getCount().done( function(json){
 			var url = json.id;
 			var count = json.shares;
-			var buttonText = "やっとさー！(" + count + ")";
-			var shareTag = ysb.composeShareTag(url, "", buttonText);
-			$(anchorNode).append(shareTag);
+			var sirText = "やっとさー！(" + count + ")";
+			var sirTag = ysb.composeShareTag(url, "", sirText);
+			$(anchorNode).append(sirTag);
+			
+			//var yetText = "やっとやっと！(" + count + ")";
+			//var yetTag = ysb.composeLikeTag(url, "", yetText);
+			//$(anchorNode).append(yetTag);
 		})
 		.fail( function(e){
 			console.log("request failed : " + e);
@@ -19,18 +23,28 @@ jQuery(document).ready( function($){
 var YetSirButton = {};
 
 // やっとさーボタン用のタグを構築して返す。
+//<a href="popup.html" onClick="javascript:window.open('popup.html','windowname','width=340,height=670');return false;">開く</a>
 YetSirButton.composeShareTag = function(url, title, text){
 	var href = 'http://www.facebook.com/sharer.php?display=popup&u=' + url + '&t=' + title;
 	var tag = $('<a>' + text  + '</a>');
 	tag.on('click', function(){
-		window.open(href,"windowname","width=670,height=340");
+		window.open(href,null,"width=670,height=340");
 	});
 	return tag;
 };
 
-//<a href="popup.html" onClick="javascript:window.open('popup.html','windowname','width=340,height=670');return false;">開く</a>
-
-
+// やっとやっとボタン用のタグを構築して返す
+//https://www.facebook.com/plugins/like.php?href=[]&width&layout=button&action=like&show_faces=true&share=false&height=35&appId=1420156181559607
+YetSirButton.composeLikeTag = function(url, title, text){
+	var href = 'http://www.facebook.com/plugins/like.php?href=' + url + '&width=80&layout=button&action=like&show_faces=true&share=false&height=35&appId=1420156181559607';
+	var tag = $('<iframe src="' + href + '" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:80px; height:35px;" allowTransparency="true">' + text  + '</a>');
+	/*
+	tag.on('click', function(){
+		window.open(href,null,"width=650,height=450");
+	});
+	*/
+	return tag;
+};
 
 // graph apiを呼び出していいね件数を取得する
 YetSirButton.getCount = function(url){
@@ -38,8 +52,8 @@ YetSirButton.getCount = function(url){
 	
 	// graph api
 	// http://graph.facebook.com/?id=http://hello-apis.blogspot.jp/2013/02/facebookapi.html
-	var url = "http://creators-db.net/entry/";
-	//var url = location.href;
+	//var url = "http://creators-db.net/entry/";
+	var url = location.href;
 	
 	var api_url = "http://graph.facebook.com/?id=" + url;
 	$.ajax({
